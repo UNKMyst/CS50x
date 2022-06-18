e91c16b071447ca6d29e013a878709534efeae02
Myst <66555345+UNKMyst@users.noreply.github.com>
2022-06-18 16:54:26 +0800

    Create recover.c

---
+++ Week04/recover.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+typedef uint8_t BYTE;
+
+int main(int argc, char *argv[])
+{
+    // Check if arguement is 2
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
+    // Open file for reading
+    FILE *input_file = fopen(argv[1], "r");
+
+    // Check if input_file is valid
+    if (input_file == NULL)
+    {
+        printf("Could not open file");
+        return 2;
+    }
+
+    // Store 512 bytes in an array
+    unsigned char buffer[512];
+
+    // Track number of images generated
+    int imageCount = 0;
+
+    // File pointer for recovered images
+    FILE *output_file = NULL;
+
+    // char filename of 8
+    char *filename = malloc(8 * sizeof(char));
+
+    // read blocks of 512 bytes
+    while (fread(buffer, sizeof(char), 512, input_file))
+    {
+        // check if bytes is JPEG
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // Write JPEG filenames
+            sprintf(filename, "%03u.jpg", imageCount);
+
+            // open output_file for writing
+            output_file = fopen(filename, "w");
+
+            // Count number of images
+            imageCount++;
+        }
+        // Check if output is valid
+        if (output_file != NULL)
+        {
+            fwrite(buffer, sizeof(char), 512, output_file);
+        }
+    }
+    // free memory
+    free(filename);
+    fclose(output_file);
+    fclose(input_file);
+
+    return 0;
+}
