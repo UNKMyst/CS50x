da92dc7941bbeb35e076cb87dc0e41c21849ca95
Myst <66555345+UNKMyst@users.noreply.github.com>
2022-07-05 11:33:22 +0800

    Create Dictionary.c

---
+++ Week05/Dictionary.c
@@ -0,0 +1,144 @@
+// Made by Paclibar, Josh Using CS50x Libraries/Files
+
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+// Include additional Libraries
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+
+// Declared Variabls
+unsigned int word_count;
+unsigned int hash_value;
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    // Hash word to obtain hash value
+    hash_value = hash(word);
+
+    // Point Cursor to the first node
+    node *cursor = table[hash_value];
+
+    // Go though the list
+    while (cursor != 0)
+    {
+        if (strcasecmp(word, cursor->word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // TODO: Improve this hash function
+    unsigned long total = 0;
+    for (int i = 0; i < strlen(word); i++)
+    {
+        total += tolower(word[i]);
+    }
+    return total % N;
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    // TODO
+    // Open Dictionary first
+    FILE *file = fopen(dictionary, "r");
+
+    // If not open return null
+    if (file == NULL)
+    {
+        printf("Unable to open %s", dictionary);
+        return false;
+    }
+    // Declare Var word
+    char word[LENGTH+1];
+
+    // Will Scan dictionary for String up until End of File (EOF)
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        // Allocate memory for each node
+        node *n = malloc(sizeof(node));
+
+        // if malloc is NULL, return false
+        if (n == NULL)
+        {
+            return false;
+        }
+
+        // Copy word into node
+        strcpy(n->word, word);
+        hash_value = hash(word);
+        n->next = table[hash_value];
+        table[hash_value] = n;
+        // Increment count
+        word_count++;
+    }
+    fclose(file);
+    return true;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    if (word_count > 0)
+    {
+        return word_count;
+    }
+    return 0;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    // Iterate Through all buckets
+    for (int i = 0; i < N; i++)
+    {
+        // Set cursor to the start of linked List
+        node *cursor = table[i];
+
+        // If Cursor is not NULL, Free memory
+        while (cursor)
+        {
+            // Create tmp node
+            node *tmp = cursor;
+            // Move cursor to next node
+            cursor = cursor->next;
+            // Free tmp node
+            free(tmp);
+        }
+        if (cursor == NULL)
+        {
+             return true;
+        }
+    }
+    return false;
+}
